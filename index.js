62809496dcd5845832716bfa998d86c8b8c8b7a2
Alice Wenban <wenbanalice@gmail.com>
2018-06-25 21:43:52 +0100

    Added loop

--- index.js
+++ index.js
@@ -2,25 +2,40 @@ const readline = require('readline-sync');
 console.log('Welcome to the calculator! \n\ ==========================');
 console.log('Please enter the operator:');
 const op = readline.prompt();
-console.log('Please enter the first number:');
-const numa = parseInt(readline.prompt());
-console.log('Please enter the second number:');
-const numb = parseInt(readline.prompt());
-var result;
-switch(op){
-    case'+':
-        result= numa+numb;
-    break;
-    case'*':
-        result= numa*numb;
-    break;
-    case'/':
-       result= numa/numb;
-    break;
-    case'-':
-        result= numa+numb;
-    break;
+console.log('How many numbers do you want to ' +op+ '?')
+const rep = readline.prompt();
 
+var arr=Array(rep)
+for (i = 0; i <= rep; i++) {
+    var num = i+1;
+    console.log('Please enter number '+ num +':');
+    arr[i]=  parseInt(readline.prompt());
+}
+
+function performop(a,op,b) {
+    switch(op){
+        case'+':
+            a=a+b ;
+        break;
+        case'*':
+            a=a*b;
+        break;
+        case'/':
+        a= a/b;
+        break;
+        case'-':
+            a=a+b;
+        break;
+    }
+return a;
+}
+
+
+var result= arr[0];
+
+for (i = 1; i <= rep; i++) {
+   console.log(result);
+   result=performop(result,op,arr[i]);
 }
 
 
