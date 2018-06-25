f76b9e0d216ace143bf74e420e820433b34f5e0a
Alice Wenban <wenbanalice@gmail.com>
2018-06-25 21:10:26 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+const readline = require('readline-sync');
+console.log('Please enter a number:');
+const numa = parseInt(readline.prompt());
+console.log('Please enter a second number:');
+const numb = parseInt(readline.prompt());
+const mult = numa*numb;
+console.log('The result of the multiplication is: '+mult);
