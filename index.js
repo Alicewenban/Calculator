85c929cb4c3f4d280eafad42203fadd6be9ca95b
Alice Wenban <wenbanalice@gmail.com>
2018-06-25 21:20:31 +0100

    added 4 operations

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const readline = require('readline-sync');
-console.log('Please enter a number:');
+console.log('Welcome to the calculator! \n\ ==========================');
+console.log('Please enter the operator:');
+const op = readline.prompt();
+console.log('Please enter the first number:');
 const numa = parseInt(readline.prompt());
-console.log('Please enter a second number:');
+console.log('Please enter the second number:');
 const numb = parseInt(readline.prompt());
-const mult = numa*numb;
-console.log('The result of the multiplication is: '+mult);
+var result;
+switch(op){
+    case'+':
+        result= numa+numb;
+    break;
+    case'*':
+        result= numa*numb;
+    break;
+    case'/':
+       result= numa/numb;
+    break;
+    case'-':
+        result= numa+numb;
+    break;
+
+}
+
+
+console.log('The answer is: '+result);
